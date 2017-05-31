b90530061131d807d706ffed23367a132cc8d711
mike <verysimple@gmail.com>
2017-05-31 20:05:14 +0700

    improvement: add args and kwargs to create_app()

--- tsk/admin.py
+++ tsk/admin.py
@@ -16,8 +16,8 @@ Pages
 - add
 """
 
-def create_app(import_module):
-    app = flask.Flask(import_module)
+def create_app(import_module, *a, **kw):
+    app = flask.Flask(import_module, *a, **kw)
     return app
 
 def list_md_files(mdpath):
